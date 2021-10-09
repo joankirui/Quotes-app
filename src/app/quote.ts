33b5c09ab82337322f86c19d8bf3ee34d6b76012
joankirui <joankirui99@gmail.com>
2021-10-10 00:10:18 +0300

    add property  upvotes and initialize it with a value of zero

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,10 @@
 export class Quote {
     showAuthor: boolean;
+    upvotes!: number;
     constructor(public id: number,public name: string,public author: string,public personSubmitted: string,public dateCreated:Date){
         this.showAuthor=false;
+        this.upvotes=0;
+        
     }
 
 

ec8906fa4bcc9cfc6b159ce7398bf63dc5c58b1d
joankirui <joankirui99@gmail.com>
2021-10-10 00:11:45 +0300

    add property downvote and initialize it with zero

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,12 @@
 export class Quote {
     showAuthor: boolean;
     upvotes!: number;
+    downvotes!: number;
     constructor(public id: number,public name: string,public author: string,public personSubmitted: string,public dateCreated:Date){
         this.showAuthor=false;
         this.upvotes=0;
-        
+        this.downvotes=0;
+
     }
 
 

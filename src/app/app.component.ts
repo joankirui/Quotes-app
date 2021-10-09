7209fd0f557a2362f9bb11c8a20d0ba943086caa
joankirui <joankirui99@gmail.com>
2021-10-09 20:30:41 +0300

    create a component called quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes:Quote[] = [
+    {id:1, name:'Yours is the only opinion of you that matters', author:'Lala'},
+    {id:2, name:'Tomorrow comes', author:'Martha'},
+    {id:3, name:'Leap and a net will appear', author:'Frank'},
+    {id:4, name:'Optimism is an occupational hazard of programming', author:'Kent'},
+  ];
 }

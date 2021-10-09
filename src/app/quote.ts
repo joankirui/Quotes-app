631140d93c196734b126b8d39efb17d540af8c09
joankirui <joankirui99@gmail.com>
2021-10-10 00:05:05 +0300

    pass quote properties to the constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,8 @@
 export class Quote {
-    id!: number;
-    name!: string;
-    author!: string;
+    showAuthor: boolean;
+    constructor(public id: number,public name: string,public author: string,public personSubmitted: string,public dateCreated:Date){
+        this.showAuthor=false;
+    }
 
 
 }

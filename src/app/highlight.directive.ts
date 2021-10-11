d1b1e9623b8d616f57ac74d4025306290c560727
joankirui <joankirui99@gmail.com>
2021-10-11 13:31:30 +0300

    add two event handlers that respond when the mouse enters or leaves

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) { }
+  constructor(private elem:ElementRef) { 
+    this.elem.nativeElement.style.backgroundColor='yellow';
+  }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+  
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+  
+  private highlight(color: string) {
+    this.elem.nativeElement.style.backgroundColor = color;
+  }
 
 }

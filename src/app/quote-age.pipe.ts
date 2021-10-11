1f604360c3a9c628c477e05bf86c6afd67f6000f
joankirui <joankirui99@gmail.com>
2021-10-11 17:31:57 +0300

    modify the button to emit a click output event that calls the quotedelete()

--- src/app/quote-age.pipe.ts
+++ src/app/quote-age.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class QuoteAgePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let currentDay:Date = new Date();//gets current date
+    let todayWithNoTime:any = new Date(currentDay.getFullYear(), currentDay.getMonth(), currentDay.getDate());
+
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+    if (dateCounter >= 1 && value < todayWithNoTime){
+      return dateCounter;
+    } else{
+      return 0;
+    }
+
   }
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1, 'Comfort in life is fueled by money', 'Author: Jean-Pierre' ,'Submitted by:Joan', new Date(2020,2,14)),
-    new Quote(2, 'Yours is the only opinion of you that matters' ,'Author: Kent Beck','Submitted by: Jacky', new Date(2021,3,8)),
-    new Quote(3, 'Who you are tomorrow begins with what you do today' ,'Author: Michelle Ntalami', 'Submitted by: Yusuf', new Date(2019,0,12)),
-    new Quote(4, 'Leap and a net will appear' ,'Submitted by: Martha','Author: Kent Beck', new Date(2022,3,29)),
-    new Quote(5, 'Optimism is an occupational hazard of programming' ,'Author: Kent Beck', 'Submitted by: Winny', new Date(2020,11,12)),
+    new Quote(1, 'Comfort in life is fueled by money', 'Jean-Pierre' ,'Joan', new Date(2020,2,14)),
+    new Quote(2, 'Yours is the only opinion of you that matters' ,'Kent Beck','Jacky', new Date(2021,3,8)),
+    new Quote(3, 'Who you are tomorrow begins with what you do today' ,'Michelle Ntalami', 'Yusuf', new Date(2019,0,12)),
+    new Quote(4, 'Leap and a net will appear' ,'Martha','Kent Beck', new Date(2022,3,29)),
+    new Quote(5, 'Optimism is an occupational hazard of programming' ,'Kent Beck', 'Winny', new Date(2020,11,12)),
   ];
   toggleDetails(index: number){
     this.quotes[index].showPerson = !this.quotes[index].showPerson;
